dcd224e8748888aedd76906f334b1a5099ab060e
Matthieu Moreau <matthieu.moreau@pasqal.io>
2021-07-15 11:06:46 +0200

    Doc/ Add docstring ot batch and job classes

--- sdk/batch.py
+++ sdk/batch.py
@@ -11,11 +11,28 @@ class DeviceType(Enum):
     MOCK = "MOCK_DEVICE"
 
 
-# TODO: docstring
-
-
 @dataclass
 class Batch:
+    """Class for batch data.
+
+    A batch groups up several jobs with the same sequence. When a batch is assigned to a QPU,
+    all its jobs are ran sequentially and no other batch can be assigned to the device
+    until all its jobs are done and it is declared complete.
+
+    Attributes:
+        - complete: Whether the batch has been declared as complete.
+        - created_at: Timestamp of the creation of the batch.
+        - updated_at: Timestamps of the last update of the batch.
+        - device_type: Type of device To run the batch on.
+        - group_id: Id of the owner group of the batch.
+        - id: Unique identifier for the batch
+        - priority: Level of priority of the batch
+        - status: Status of the batch
+        - webhook: Webhook where the job results are automatically sent to.
+        - client: Client object for sending data to PCS API.
+        - jobs: Dictionnary of all the jobs added to the batch.
+    """
+
     complete: bool
     created_at: str
     updated_at: str
@@ -29,6 +46,15 @@ class Batch:
     jobs: Dict[int, Job] = field(default_factory=lambda: {})
 
     def add_job(self, runs: int = 100, variables: Dict = None, wait: bool = False):
+        """Add and send a new job for this batch.
+
+        Args:
+            - runs: number of times the job is ran on the QPU.
+            - variables (optional): values for variables if sequence is parametrized.
+
+        Returns:
+            - Job: the created job.
+        """
         job_rsp = self.client._send_job(
             {"runs": runs, "variables": variables, "batch_id": self.id}
         )
@@ -40,5 +66,10 @@ class Batch:
         return job
 
     def declare_complete(self) -> Dict:
+        """Declare to PCS that the batch is complete.
+
+        A batch that is complete awaits no extra jobs. The batch is then unassigned to its running
+        device when all its jobs are done.
+        """
         self.complete = True
         return self.client._complete_batch(self.id)
--- sdk/job.py
+++ sdk/job.py
@@ -3,6 +3,19 @@ from dataclasses import dataclass
 
 @dataclass
 class Job:
+    """Class for job data.
+
+    Attributes:
+        - runs: Number of time the job should be ran.
+        - created_at: Timestamp of the creation of the batch.
+        - updated_at: Timestamps of the last update of the batch.
+        - batch_id: Id of the batch which the job belongs to.
+        - result: Result of the job.
+        - errors: Error messages that occured while processing job.
+        - id: Unique identifier for the batch
+        - status: Status of the job
+    """
+
     runs: int
     batch_id: int
     id: int
