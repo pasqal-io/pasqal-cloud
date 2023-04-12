c0808f8810ea7c2fd2a4f112b17f197746c6ff62
Milan Viricel <viricel.milan@gmail.com>
2023-04-12 11:18:06 +0200

    Review comments

---
+++ sdk/__init__.py
@@ -0,0 +1,5 @@
+import warnings
+warnings.warn("cloud-sdk package is deprecated, please use pasqal_cloud instead: `pip install pasqal-cloud`",
+              DeprecationWarning)
+
+from pasqal_cloud import *
