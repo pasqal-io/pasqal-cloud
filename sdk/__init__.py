4ce68076fc1a6b2e89e0ed7c7da17cf31a4f7777
Augustinio <augustinlg.dev@gmail.com>
2023-04-14 14:41:23 +0200

    :art: formatting

--- sdk/__init__.py
+++ sdk/__init__.py
@@ -1,5 +1,8 @@
 import warnings
-warnings.warn("cloud-sdk package is deprecated, please use pasqal_cloud instead: `pip install pasqal-cloud`",
-              DeprecationWarning)
+
+warnings.warn(
+    "cloud-sdk package is deprecated, please use pasqal_cloud instead: `pip install pasqal-cloud`",
+    DeprecationWarning,
+)
 
 from pasqal_cloud import *
