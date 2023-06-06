d8c1f836f0ebbe999021d516ebedd9e2c2aa5012
Matthieu Moreau <matthieu.moreau@pasqal.io>
2023-06-06 20:36:16 +0200

    [TEST] Test that Batch and Job can be instantiated even if API returns unknown fields

--- tests/test_batch.py
+++ tests/test_batch.py
@@ -201,3 +201,19 @@ class TestBatch:
             configuration=configuration,
         )
         assert batch.configuration == expected
+
+    def test_batch_instanciation_with_extra_field(self, batch):
+        """Instantiating a batch with an extra field should not raise an error.
+
+        This enables us to add new fields in the API response on the batches endpoint
+        without breaking compatibility for users with old versions of the SDK where
+        the field is not present in the Batch class.
+        """
+        batch_dict = batch.dict()  # Batch data expected by the SDK
+        # We add an extra field to mimick the API exposing new values to the user
+        batch_dict["new_field"] = "any_value"
+
+        new_batch = Batch(**batch_dict)  # this should raise no error
+        assert (
+            new_batch.new_field == "any_value"
+        )  # The new value should be stored regardless
