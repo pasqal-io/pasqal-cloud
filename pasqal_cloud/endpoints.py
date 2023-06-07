35dac98497680e9889c3a79116dbd78110aab168
Milan Viricel <viricel.milan@gmail.com>
2023-06-07 14:55:43 +0200

    endpoints config remade

--- pasqal_cloud/endpoints.py
+++ pasqal_cloud/endpoints.py
@@ -11,7 +11,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
 from sys import version_info
 
 from dataclasses import dataclass
@@ -22,23 +21,69 @@ else:
     from typing_extensions import Final  # type: ignore
 
 
-CORE_API_URL: Final[str] = "https://apis.pasqal.cloud/core-fast"
+# ---- Endpoints ----
+
+PROD_CORE_API_URL: Final[str] = "https://apis.pasqal.cloud/core-fast"
+PREPROD_CORE_API_URL: Final[str] = "https://apis.preprod.pasqal.cloud/core-fast"
+DEV_CORE_API_URL: Final[str] = "https://apis.dev.pasqal.cloud/core-fast"
 
 
 @dataclass
 class Endpoints:
-    core: str = CORE_API_URL
+    core: str = PROD_CORE_API_URL
 
 
-AUTH0_DOMAIN: Final[str] = "pasqal.eu.auth0.com"
-PUBLIC_CLIENT_ID: Final[str] = "PeZvo7Atx7IVv3iel59asJSb4Ig7vuSB"
-AUDIENCE: Final[str] = "https://apis.pasqal.cloud/account/api/v1"
+# ---- Auth0 ----
+
+PROD_AUTH0_DOMAIN: Final[str] = "pasqal.eu.auth0.com"
+PROD_PUBLIC_CLIENT_ID: Final[str] = "PeZvo7Atx7IVv3iel59asJSb4Ig7vuSB"
+PROD_AUDIENCE: Final[str] = "https://apis.pasqal.cloud/account/api/v1"
+
+PREPROD_AUTH0_DOMAIN: Final[str] = "pasqal-preprod.eu.auth0.com"
+PREPROD_PUBLIC_CLIENT_ID: Final[str] = "2l6A2ldvwJE5sdkghu40BTYLm7sSUAv9"
+PREPROD_AUDIENCE: Final[str] = "https://apis.preprod.pasqal.cloud/account/api/v1"
+
+DEV_AUTH0_DOMAIN: Final[str] = "pasqal-dev.eu.auth0.com"
+DEV_PUBLIC_CLIENT_ID: Final[str] = "5QtfSu1UV118Iz6By6IJRSNoDrLbAiOv"
+DEV_AUDIENCE: Final[str] = "https://apis.dev.pasqal.cloud/account/api/v1"
+
 REALM: Final[str] = "pcs-users"
 
 
 @dataclass
 class Auth0Conf:
-    domain: str = AUTH0_DOMAIN
-    public_client_id: str = PUBLIC_CLIENT_ID
-    audience: str = AUDIENCE
+    domain: str = PROD_AUTH0_DOMAIN
+    public_client_id: str = PROD_PUBLIC_CLIENT_ID
+    audience: str = PROD_AUDIENCE
     realm: str = REALM
+
+
+# ---- Pre-made environment configuration ----
+
+
+PASQAL_ENDPOINTS = {
+    "prod": Endpoints(core=PROD_CORE_API_URL),
+    "preprod": Endpoints(core=PREPROD_CORE_API_URL),
+    "dev": Endpoints(core=DEV_CORE_API_URL),
+}
+
+AUTH0_CONFIG = {
+    "prod": Auth0Conf(
+        domain=PROD_AUTH0_DOMAIN,
+        public_client_id=PROD_PUBLIC_CLIENT_ID,
+        audience=PROD_AUDIENCE,
+        realm=REALM,
+    ),
+    "preprod": Auth0Conf(
+        domain=PREPROD_AUTH0_DOMAIN,
+        public_client_id=PREPROD_PUBLIC_CLIENT_ID,
+        audience=PREPROD_AUDIENCE,
+        realm=REALM,
+    ),
+    "dev": Auth0Conf(
+        domain=DEV_AUTH0_DOMAIN,
+        public_client_id=DEV_PUBLIC_CLIENT_ID,
+        audience=DEV_AUDIENCE,
+        realm=REALM,
+    ),
+}
