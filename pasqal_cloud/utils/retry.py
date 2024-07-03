a922341d21dfd449fe27c566e7451b9dd054895d
Amin <mohamed.mallek@pasqal.com>
2024-07-03 09:05:57 +0000

    replace type with typing.Type

--- pasqal_cloud/utils/retry.py
+++ pasqal_cloud/utils/retry.py
@@ -1,6 +1,6 @@
 import functools
 import time
-from typing import Callable, Iterable, Optional, Tuple, TypeVar
+from typing import Callable, Iterable, Optional, Tuple, Type, TypeVar
 
 from requests import HTTPError
 from typing_extensions import ParamSpec
@@ -12,7 +12,7 @@ RT = TypeVar("RT")  # return type
 def retry_http_error(
     max_retries: int = 5,
     retry_status_code: Optional[Iterable[int]] = None,
-    retry_exceptions: Optional[Tuple[type[Exception]]] = None,
+    retry_exceptions: Optional[Tuple[Type[Exception]]] = None,
 ) -> Callable[[Callable[..., RT]], Callable[..., RT]]:
     """
     Decorator to retry an HTTP call when an HTTPError is encountered.
