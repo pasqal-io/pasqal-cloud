ed3c3a2afcf698ea14cc13e68de3389ee70834f8
oliver.gordon <oliver.gordon@pasqal.com>
2024-09-09 17:08:12 +0200

    emu fresnel access in SDK

--- pasqal_cloud/device/configuration/__init__.py
+++ pasqal_cloud/device/configuration/__init__.py
@@ -3,11 +3,13 @@ from pasqal_cloud.device.configuration.base_config import (
     InvalidConfiguration,
 )
 from pasqal_cloud.device.configuration.emu_free import EmuFreeConfig
+from pasqal_cloud.device.configuration.emu_fresnel import EmuFresnelConfig
 from pasqal_cloud.device.configuration.emu_tn import EmuTNConfig
 
 __all__ = [
     "BaseConfig",
     "EmuFreeConfig",
+    "EmuFresnelConfig",
     "EmuTNConfig",
     "InvalidConfiguration",
 ]
