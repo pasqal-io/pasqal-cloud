75107c08a0930f257873903afad873ebc1657aa9
Milan Viricel <milan.viricel@pasqal.com>
2025-01-07 10:04:52 +0100

    review

--- pasqal_cloud/device/configuration/__init__.py
+++ pasqal_cloud/device/configuration/__init__.py
@@ -4,14 +4,12 @@ from pasqal_cloud.device.configuration.base_config import (
 )
 from pasqal_cloud.device.configuration.emu_free import EmuFreeConfig
 from pasqal_cloud.device.configuration.emu_fresnel import EmuFresnelConfig
-from pasqal_cloud.device.configuration.emu_mps import EmuMpsConfig
 from pasqal_cloud.device.configuration.emu_tn import EmuTNConfig
 
 __all__ = [
     "BaseConfig",
     "EmuFreeConfig",
     "EmuFresnelConfig",
-    "EmuMpsConfig",
     "EmuTNConfig",
     "InvalidConfiguration",
 ]
--- pasqal_cloud/device/configuration/emu_mps.py
+++
@@ -1,8 +0,0 @@
-from __future__ import annotations
-
-from dataclasses import dataclass
-
-
-@dataclass
-class EmuMpsConfig:
-    """Configuration for the EMU_MPS device type."""
