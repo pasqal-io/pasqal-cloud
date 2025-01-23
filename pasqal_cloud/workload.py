df493fa9b6ed2d0a0fcb8fed899133be347f4b3f
Benjamin Becquet <benjamin.becquet@pasqal.com>
2025-01-23 17:20:36 +0100

    Fix docstring syntax

--- pasqal_cloud/workload.py
+++ pasqal_cloud/workload.py
@@ -21,22 +21,22 @@ class Workload(BaseModel):
     """Class for workload data.
 
     Attributes:
-        - id: Unique identifier for the workload.
-        - project_id: ID of the project which the users scheduling the workload
-         belong to.
-        - status: Status of the workload. Possible values are:
-            PENDING, RUNNING, DONE, CANCELED, TIMED_OUT, ERROR, PAUSED.
-        - _client: A Client instance to connect to PCS.
-        - backend: The backend used for the workload.
-        - workload_type: The type of the workload.
-        - config: The config containing all the necessary information for
-         the workload to run.
-        - created_at: Timestamp of the creation of the workload.
-        - updated_at: Timestamp of the last update of the workload.
-        - errors: Error messages that occurred while processing workload.
-        - start_timestamp: The timestamp of when the workload began processing.
-        - end_timestamp: The timestamp of when the workload finished processing.
-        - result: Result of the workload.
+        id: Unique identifier for the workload.
+        project_id: ID of the project which the users scheduling the workload
+        belong to.
+        status: Status of the workload. Possible values are:
+          PENDING, RUNNING, DONE, CANCELED, TIMED_OUT, ERROR, PAUSED.
+        _client: A Client instance to connect to PCS.
+        backend: The backend used for the workload.
+        workload_type: The type of the workload.
+        config: The config containing all the necessary information for
+        the workload to run.
+        created_at: Timestamp of the creation of the workload.
+        updated_at: Timestamp of the last update of the workload.
+        errors: Error messages that occurred while processing workload.
+        start_timestamp: The timestamp of when the workload began processing.
+        end_timestamp: The timestamp of when the workload finished processing.
+        result: Result of the workload.
     """
 
     id: str
