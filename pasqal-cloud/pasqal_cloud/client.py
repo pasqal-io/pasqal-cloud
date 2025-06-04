f2f50f1bc94aa05d940335a17dd88c7d7ec64541
David Teller <david.teller@pasqal.com>
2025-06-04 11:30:25 +0200

    Separate async client

--- pasqal-cloud/pasqal_cloud/client.py
+++ pasqal-cloud/pasqal_cloud/client.py
@@ -476,3 +476,431 @@ class Client:
             json = await response.json()
             devices = json["data"]
             return {device["device_type"]: device["specs"] for device in devices}
+
+
+class AsyncClient:
+    def __init__(
+        self,
+        project_id: str | None = None,
+        username: Optional[str] = None,
+        password: Optional[str] = None,
+        token_provider: Optional[TokenProvider] = None,
+        endpoints: Optional[Endpoints] = None,
+        auth0: Optional[Auth0Conf] = None,
+    ):
+        self.endpoints = self._make_endpoints(endpoints)
+        self._project_id = project_id
+        self.user_agent = f"PasqalCloudSDK/{sdk_version}"
+
+        if token_provider is not None:
+            self._check_token_provider(token_provider)
+
+        if username:
+            auth0 = self._make_auth0(auth0)
+            token_provider = self._credential_login(username, password, auth0)
+
+        self.authenticator: Optional[HTTPBearerAuthenticator] = None
+        if token_provider:
+            self.authenticator = HTTPBearerAuthenticator(token_provider)
+
+    def user_token(self) -> Union[str, None]:
+        return (
+            self.authenticator.token_provider.get_token()  # type: ignore[attr-defined]
+            if self.authenticator
+            else None
+        )
+
+    @property
+    def project_id(self) -> str:
+        if not self._project_id:
+            raise ValueError("You need to set a project_id.")
+        return self._project_id
+
+    @project_id.setter
+    def project_id(self, project_id: str) -> None:
+        self._project_id = project_id
+
+    @staticmethod
+    def _make_endpoints(endpoints: Optional[Endpoints]) -> Endpoints:
+        if endpoints is None:
+            return Endpoints()
+
+        if not isinstance(endpoints, Endpoints):
+            raise TypeError(f"endpoints must be a {Endpoints.__name__} instance")
+
+        return endpoints
+
+    @staticmethod
+    def _make_auth0(auth0: Optional[Auth0Conf]) -> Auth0Conf:
+        if auth0 is None:
+            return Auth0Conf()
+
+        if not isinstance(auth0, Auth0Conf):
+            raise TypeError(f"auth0 parameter must be a {Auth0Conf.__name__} instance")
+
+        return auth0
+
+    @staticmethod
+    def _check_token_provider(token_provider: TokenProvider) -> None:
+        if not isinstance(token_provider, TokenProvider):
+            raise TypeError("token_provider must be an instance of TokenProvider.")
+
+    def _credential_login(
+        self, username: str, password: Optional[str], auth0: Auth0Conf
+    ) -> TokenProvider:
+        if not password:
+            password = getpass("Enter your password:")
+            #   We want to allow an empty string at first, but then
+            #   it results in an error
+            if not password:
+                raise ValueError("The prompted password should not be empty")
+
+        token_provider: TokenProvider = Auth0TokenProvider(username, password, auth0)
+        return token_provider
+
+    @staticmethod
+    async def _async_request_with_status_check(*args: Any, **kwargs: Any):  # type: ignore
+        async with aiohttp.request(*args, **kwargs) as resp:
+            resp.raise_for_status()
+            return resp
+
+    @staticmethod
+    def _request_with_status_check(*args: Any, **kwargs: Any):  # type: ignore
+        resp = requests.request(*args, **kwargs)
+        resp.raise_for_status()
+        return resp
+
+    def _authenticated_request(
+        self,
+        method: str,
+        url: str,
+        payload: Optional[Union[Mapping, Sequence[Mapping]]] = None,
+        params: Optional[Mapping[str, Any]] = None,
+    ) -> JSendPayload:
+        if self.authenticator is None:
+            raise ValueError(
+                "Authentication required. Please provide your credentials when"
+                " initializing the client."
+            )
+
+        headers = {
+            "content-type": "application/json",
+            "User-Agent": self.user_agent,
+        }
+
+        if method == "GET":
+            request_with_retry = retry_http_error(
+                max_retries=5,
+                retry_status_code={408, 425, 429, 500, 502, 504},
+                retry_exceptions=(requests.ConnectionError, requests.Timeout),  # type: ignore
+            )(self._request_with_status_check)
+        else:
+            request_with_retry = retry_http_error(
+                max_retries=5,
+                retry_status_code={408, 425, 429, 500, 502, 504},
+            )(self._request_with_status_check)
+
+        resp = request_with_retry(
+            method,
+            url,
+            json=payload,
+            timeout=TIMEOUT,
+            headers=headers,
+            auth=self.authenticator,
+            params=params,
+        )
+        data: JSendPayload = resp.json()
+        return data
+
+    async def _async_authenticated_request(
+        self,
+        method: str,
+        url: str,
+        payload: Optional[Union[Mapping, Sequence[Mapping]]] = None,
+        params: Optional[Mapping[str, Any]] = None,
+    ) -> JSendPayload:
+        if self.authenticator is None:
+            raise ValueError(
+                "Authentication required. Please provide your credentials when"
+                " initializing the client."
+            )
+
+        headers = {
+            "content-type": "application/json",
+            "User-Agent": self.user_agent,
+        }
+
+        if method == "GET":
+            request_with_retry = async_retry_http_error(
+                max_retries=5,
+                retry_status_code={408, 425, 429, 500, 502, 504},
+                retry_exceptions=(requests.ConnectionError, requests.Timeout),  # type: ignore
+            )(self._async_request_with_status_check)
+        else:
+            request_with_retry = async_retry_http_error(
+                max_retries=5,
+                retry_status_code={408, 425, 429, 500, 502, 504},
+            )(self._async_request_with_status_check)
+
+        resp = await request_with_retry(
+            method,
+            url,
+            json=payload,
+            timeout=TIMEOUT,
+            headers=headers,
+            auth=self.authenticator,
+            params=params,
+        )
+        data: JSendPayload = await resp.json()
+        return data
+
+    def _request_all_pages(
+        self,
+        method: str,
+        url: str,
+        payload: Optional[Union[Mapping, Sequence[Mapping]]] = None,
+        params: Optional[Dict[str, Any]] = None,
+    ) -> List[Dict[str, Any]]:
+        """
+        Request all pages of a paginated endpoint and return a list of all items from
+        the requested pages
+
+        Args:
+            method: HTTP method
+            url: requested endpoint url
+            payload: query payload
+            params: query params
+
+        Returns:
+            A list of items from the requested pages.
+        """
+
+        if not params:
+            params = {}
+        first_page_response = self._authenticated_request(
+            method=method, url=url, payload=payload, params=params
+        )
+        all_items: List[Dict] = first_page_response["data"]
+        pagination_data = first_page_response.get("pagination")
+
+        if not pagination_data:
+            return all_items
+
+        total_nb_items = pagination_data["total"]
+        end = pagination_data["end"]
+        while end < total_nb_items:
+            params["offset"] = end
+            response: JSendPayload = self._authenticated_request(
+                method=method, url=url, payload=payload, params=params
+            )
+
+            all_items.extend(response["data"])
+            pagination_data = response.get("pagination")
+
+            # Added to make sure the type is correctly set for mypy
+            # Should not happen
+            if not pagination_data:
+                break
+
+            total_nb_items = pagination_data["total"]
+            end = pagination_data["end"]
+        return all_items
+
+    def send_batch(self, batch_data: Dict[str, Any]) -> Dict[str, Any]:
+        batch_data.update({"project_id": self.project_id})
+        response: Dict[str, Any] = self._authenticated_request(
+            "POST",
+            f"{self.endpoints.core}/api/v1/batches",
+            batch_data,
+        )["data"]
+        return response
+
+    def get_batch(self, batch_id: str) -> Dict[str, Any]:
+        response: Dict[str, Any] = self._authenticated_request(
+            "GET", f"{self.endpoints.core}/api/v2/batches/{batch_id}"
+        )["data"]
+        return response
+
+    async def async_get_batch(self, batch_id: str) -> Dict[str, Any]:
+        response = await self._async_authenticated_request(
+            "GET", f"{self.endpoints.core}/api/v2/batches/{batch_id}"
+        )
+        result = response["data"]
+        assert isinstance(result, Dict)
+        return result
+
+    def get_batch_jobs(self, batch_id: str) -> list[Dict[str, Any]]:
+        return self._request_all_pages(
+            "GET",
+            f"{self.endpoints.core}/api/v2/jobs",
+            params={
+                "batch_id": batch_id,
+                "order_by": "ordered_id",
+                "order_by_direction": "ASC",
+            },
+            # The order-by parameters are required to ensure that the jobs appear in
+            # the same order as they do in the GET /batches/{id} response
+        )
+
+    @retry_http_error(max_retries=5, retry_exceptions=(requests.ConnectionError,))
+    def _download_results(self, results_link: str) -> JobResult:
+        response = requests.request("GET", results_link)
+        response.raise_for_status()
+        data = response.json()
+        return JobResult(
+            raw=data.pop("raw", None), counter=data.pop("counter", None), **data
+        )
+
+    def get_job_results(self, job_id: str) -> Optional[JobResult]:
+        """
+        Download job result from S3.
+
+        This function request a presigned-url for a specific job_id from the core API.
+        Once the presigned-url is obtained, it downloads the result from S3.
+        """
+        results_link = self._authenticated_request(
+            "GET", f"{self.endpoints.core}/api/v1/jobs/{job_id}/results_link"
+        )["data"]["results_link"]
+        if results_link:
+            return self._download_results(results_link)
+        return None
+
+    def close_batch(self, batch_id: str) -> Dict[str, Any]:
+        response: Dict[str, Any] = self._authenticated_request(
+            "PATCH", f"{self.endpoints.core}/api/v2/batches/{batch_id}/complete"
+        )["data"]
+        return response
+
+    def cancel_batch(self, batch_id: str) -> Dict[str, Any]:
+        response: Dict[str, Any] = self._authenticated_request(
+            "PATCH", f"{self.endpoints.core}/api/v2/batches/{batch_id}/cancel"
+        )["data"]
+        return response
+
+    def cancel_batches(self, batch_ids: List[str]) -> Dict[str, Any]:
+        response: Dict[str, Any] = self._authenticated_request(
+            "PATCH",
+            f"{self.endpoints.core}/api/v1/batches/cancel",
+            payload={"batch_ids": batch_ids},
+        )["data"]
+        return response
+
+    def rebatch(
+        self, batch_id: Union[UUID, str], filters: RebatchFilters
+    ) -> Dict[str, Any]:
+        response: Dict[str, Any] = self._authenticated_request(
+            "POST",
+            f"{self.endpoints.core}/api/v1/batches/{batch_id}/rebatch",
+            params=filters.model_dump(exclude_unset=True),
+        )["data"]
+        return response
+
+    def get_jobs(
+        self, filters: JobFilters, pagination_params: PaginationParams
+    ) -> JSendPayload:
+        filters_params = filters.model_dump(exclude_unset=True)
+        filters_params.update(pagination_params.model_dump())
+        response: JSendPayload = self._authenticated_request(
+            "GET",
+            f"{self.endpoints.core}/api/v2/jobs",
+            params=filters_params,
+        )
+        return response
+
+    def get_batches(
+        self, filters: BatchFilters, pagination_params: PaginationParams
+    ) -> JSendPayload:
+        filters_params = filters.model_dump(exclude_unset=True)
+        filters_params.update(pagination_params.model_dump())
+        response: JSendPayload = self._authenticated_request(
+            "GET",
+            f"{self.endpoints.core}/api/v1/batches",
+            params=filters_params,
+        )
+        return response
+
+    def add_jobs(
+        self, batch_id: str, jobs_data: Sequence[Mapping[str, Any]]
+    ) -> Dict[str, Any]:
+        response: Dict[str, Any] = self._authenticated_request(
+            "POST", f"{self.endpoints.core}/api/v2/batches/{batch_id}/jobs", jobs_data
+        )["data"]
+        return response
+
+    def get_job(self, job_id: str) -> Dict[str, Any]:
+        response: Dict[str, Any] = self._authenticated_request(
+            "GET", f"{self.endpoints.core}/api/v2/jobs/{job_id}"
+        )["data"]
+        return response
+
+    async def async_get_job(self, job_id: str) -> Dict[str, Any]:
+        response = await self._async_authenticated_request(
+            "GET", f"{self.endpoints.core}/api/v2/jobs/{job_id}"
+        )
+        result = response["data"]
+        assert isinstance(result, Dict)
+        return result
+
+    def cancel_job(self, job_id: str) -> Dict[str, Any]:
+        response: Dict[str, Any] = self._authenticated_request(
+            "PATCH", f"{self.endpoints.core}/api/v2/jobs/{job_id}/cancel"
+        )["data"]
+        return response
+
+    def cancel_jobs(
+        self, batch_id: Union[UUID, str], filters: CancelJobFilters
+    ) -> Dict[str, Any]:
+        response: Dict[str, Any] = self._authenticated_request(
+            "PATCH",
+            f"{self.endpoints.core}/api/v2/batches/{batch_id}/cancel/jobs",
+            params=filters.model_dump(exclude_unset=True),
+        )["data"]
+        return response
+
+    def send_workload(self, workload_data: Dict[str, Any]) -> Dict[str, Any]:
+        workload_data.update({"project_id": self.project_id})
+        response: Dict[str, Any] = self._authenticated_request(
+            "POST",
+            f"{self.endpoints.core}/api/v1/workloads",
+            workload_data,
+        )["data"]
+        return response
+
+    def get_workload(self, workload_id: str) -> Dict[str, Any]:
+        response: Dict[str, Any] = self._authenticated_request(
+            "GET", f"{self.endpoints.core}/api/v2/workloads/{workload_id}"
+        )["data"]
+        return response
+
+    def cancel_workload(self, workload_id: str) -> Dict[str, Any]:
+        response: Dict[str, Any] = self._authenticated_request(
+            "PUT", f"{self.endpoints.core}/api/v1/workloads/{workload_id}/cancel"
+        )["data"]
+        return response
+
+    def get_device_specs_dict(self) -> Dict[str, str]:
+        # FIXME: Port to async
+        if self.authenticator is not None:
+            response: Dict[str, str] = self._authenticated_request(
+                "GET", f"{self.endpoints.core}/api/v1/devices/specs"
+            )["data"]
+            return response
+        return self.get_public_device_specs()
+
+    def get_public_device_specs(self) -> Dict[str, str]:
+        response = requests.request(
+            "GET", f"{self.endpoints.core}/api/v1/devices/public-specs"
+        )
+        response.raise_for_status()
+        devices = response.json()["data"]
+        return {device["device_type"]: device["specs"] for device in devices}
+
+    async def async_get_public_device_specs(self) -> Dict[str, str]:
+        async with aiohttp.request(
+            method="GET",
+            url=f"{self.endpoints.core}/api/v1/devices/public-specs",
+        ) as response:
+            response.raise_for_status()
+            json = await response.json()
+            devices = json["data"]
+            return {device["device_type"]: device["specs"] for device in devices}
