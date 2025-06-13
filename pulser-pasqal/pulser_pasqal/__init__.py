66ccd3481fefe6a46053ae7507f4485e0320adb0
Amin <mohamed.mallek@pasqal.com>
2025-06-13 11:57:00 +0200

    EmuMpsBackend using the new backend interface

--- pulser-pasqal/pulser_pasqal/__init__.py
+++ pulser-pasqal/pulser_pasqal/__init__.py
@@ -16,7 +16,7 @@
 from pasqal_cloud import BaseConfig, EmulatorType, Endpoints  # noqa: F401
 
 from pulser_pasqal._version import __version__ as __version__
-from pulser_pasqal.backends import EmuFreeBackend, EmuTNBackend
+from pulser_pasqal.backends import EmuFreeBackend, EmuMpsBackend, EmuTNBackend
 from pulser_pasqal.pasqal_cloud import PasqalCloud
 
-__all__ = ["EmuFreeBackend", "EmuTNBackend", "PasqalCloud"]
+__all__ = ["EmuFreeBackend", "EmuTNBackend", "PasqalCloud", "EmuMpsBackend"]
