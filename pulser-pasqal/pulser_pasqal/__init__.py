b0ca05e49a262f12d544efded8598a36b70d62de
Antoine Cornillot <61453516+a-corni@users.noreply.github.com>
2025-09-04 16:00:53 +0200

    [FEAT] Enable import of EmuFreeBackendV2 from pulser_pasqal (#207)
    * Adding EmuFreeBackendV2 from direct imports

--- pulser-pasqal/pulser_pasqal/__init__.py
+++ pulser-pasqal/pulser_pasqal/__init__.py
@@ -16,12 +16,18 @@
 from pasqal_cloud import BaseConfig, EmulatorType, Endpoints  # noqa: F401
 
 from pulser_pasqal._version import __version__ as __version__
-from pulser_pasqal.backends import EmuFreeBackend, EmuMPSBackend, EmuTNBackend
+from pulser_pasqal.backends import (
+    EmuFreeBackend,
+    EmuFreeBackendV2,
+    EmuMPSBackend,
+    EmuTNBackend,
+)
 from pulser_pasqal.ovh import OVHConnection
 from pulser_pasqal.pasqal_cloud import PasqalCloud
 
 __all__ = [
     "EmuFreeBackend",
+    "EmuFreeBackendV2",
     "EmuTNBackend",
     "PasqalCloud",
     "EmuMPSBackend",
