41034e8b5c573ac23e7a8dd362c1f1f6acffd394
Amin <mohamed.mallek@pasqal.com>
2026-02-13 16:30:53 +0100

    Apply comments

--- pasqal-cloud/examples/target_sa_prod.py
+++ pasqal-cloud/examples/target_sa_prod.py
@@ -3,6 +3,6 @@ from pasqal_cloud import SDK
 username = ""
 project_id = "<project-uuid>"
 
-sdk = SDK(username=username, region="saudi-arabia")
+sdk = SDK(username=username, region="sa")
 
 sdk.get_batches()
